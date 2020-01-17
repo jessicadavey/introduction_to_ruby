968877f75391a2bd2a0ee5c10f16de30e2f1b938
Jessica Davey <jessica@jessicadavey.com>
2020-01-17 09:01:19 -0600

    exercise 4

---
+++ the_basics/exercise_4.rb
@@ -0,0 +1,13 @@
+movies = {
+  thor_ragnarok: 2017,
+  dr_strange: 2016,
+  avengers_civil_war: 2016,
+  ant_man: 2015,
+  black_panther: 2018
+}
+
+movie_years = []
+
+movies.each { |movie, year| movie_years<<year}
+
+puts movie_years
