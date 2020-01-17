ddf54375dfe980e21a035f0d1227acea6bec5e50
Jessica Davey <jessica@jessicadavey.com>
2020-01-17 12:31:13 -0600

    exercise 4

--- variables/name.rb
+++ variables/name.rb
@@ -1,5 +1,9 @@
-puts "Hello, what is your name?"
-name = gets.chomp
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+
+name = first_name + " " + last_name
 
 puts "Nice to meet you, #{name}!"
 
