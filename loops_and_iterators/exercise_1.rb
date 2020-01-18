2fe32e3b22688dd46f455dbb39cb804d498ffb5d
Jessica Davey <jessica@jessicadavey.com>
2020-01-18 14:38:32 -0600

    loops and iterators, up to the start of exercise 2

---
+++ loops_and_iterators/exercise_1.rb
@@ -0,0 +1,6 @@
+x = [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1
+end
+
+# guess: returns the original array [1, 2, 3, 4, 5] <-- correct!
---
+++ loops_and_iterators/exercise_2.rb
@@ -0,0 +1,5 @@
+
+loop do
+  puts "What should I do now?"
+  answer = gets.chomp
+
