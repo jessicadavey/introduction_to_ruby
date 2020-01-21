b450f8e31401245293ee796b3d975bfae974d5e6
Jessica Davey <jessica@jessicadavey.com>
2020-01-21 10:17:23 -0600

    finished exercise 8 of hashes

--- hashes/exercise_8.rb
+++ hashes/exercise_8.rb
@@ -2,9 +2,36 @@ words = ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
         'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide', 
         'flow', 'neon']
 
-words.each do |x|
-  anagrams = words.select do |word|
-    word.chars.sort == x.chars.sort 
+
+anagrams = []
+
+words.each do |word|
+  unless anagrams.flatten.include?(word)
+    group = words.select do |x|
+      x.chars.sort == word.chars.sort 
+    end
+    anagrams.push(group)
   end
-  p anagrams
 end
+
+anagrams.each { |array| p array }
+
+# Another solution, using a hash
+
+results = {}
+
+words.each do |word|
+  key = word.chars.sort.join.to_sym
+  if results.has_key?(key)
+    # push the word to the array for that key
+    results[key].push(word)
+  else
+    # make it a key
+    results[key] = [word]
+  end
+end
+
+results.each_value do |array|
+  p array
+end
+
