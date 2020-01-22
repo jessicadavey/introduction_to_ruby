3af35e28f9de8be74a4ed0d2f6b5feb58a187dc6
Jessica Davey <jessica@jessicadavey.com>
2020-01-21 20:17:49 -0600

    exercises chapter, up to the start of exercise 9

---
+++ exercises/exercise_7.rb
@@ -0,0 +1,6 @@
+# Difference between an Array and a Hash
+
+# In an array, the elements are in a particular order, whereas
+# in a hash they are not.  Therefore, in an array elements are accessed
+# by their position (a number), while in a hash they are accessed
+# by their key, which could be any data type.
---
+++ exercises/exercise_8.rb
@@ -0,0 +1,6 @@
+my_hash = { hello: "goodbye" }
+
+my_hash2 = { :hello => "goodbye" }
+
+p my_hash
+p my_hash2
