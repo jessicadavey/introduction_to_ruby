4a1551ef82c024f4d722ad5711ce4aa545fdd2cd
Jessica Davey <jessica@jessicadavey.com>
2020-01-22 20:09:00 -0600

    finished exercise section

--- exercises/exercise_14.rb
+++ exercises/exercise_14.rb
@@ -1,5 +1,14 @@
+
 contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
 
 contacts = {"Joe Smith" => {}}
 
 contact_keys = [:email, :address, :phone]
+
+contacts.each do |name, hash|
+  contact_keys.each do |field|
+    hash[field] = contact_data.shift
+  end
+end
+
+puts contacts
